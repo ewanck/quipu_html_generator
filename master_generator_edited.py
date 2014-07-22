31875a32313b8eded299e0c3e878dbe440fbbda5
ewanck <e.cass-kavanagh@hotmail.co.uk>
2014-07-22 13:39:02 +0100

    backing up html generator

--- master_generator_edited.py
+++ master_generator_edited.py
@@ -10,26 +10,26 @@ f = open('html/master_edited.html','w')
 
 threadid = 0;
 
-subtitles = csv.reader(file('subtitles/ESPERANZA_A_spanish.csv', 'rb'))
+subtitles = csv.reader(file('subtitles/TEODULA_C_spanish.csv', 'rb'))
 ii = 0;
-thread = "<p>"
-name = "esperanza"
-section = "a"
+thread = ""
+name = "teodula"
+section = "c"
 
 
 for subtitle in subtitles:
     if ii>0:
         time_in = str((float(subtitle[3])/24))
         time_out = str((float(subtitle[4])/24))
-        thread = thread + "<a id='thread_a_"+str(threadid)+"' class='thread "+name+" "+section+"' data-in='"+time_in+"' data-out='"+time_out+"'>"+subtitle[6]+"</a>"
+        thread = thread + "<a id='thread_ccc_"+str(threadid)+"' class='thread "+name+" "+section+"' data-in='"+time_in+"' data-out='"+time_out+"'>"+subtitle[6]+"</a>"
         threadid = threadid + 1
-        edits = csv.reader(file('edits/ESPERANZA_A.csv', 'rb'))
+        edits = csv.reader(file('edits/TEODULA_C.csv', 'rb'))
         for edit in edits:
             print edit[0]
             print ii
             if edit[0]==str(ii):
                 print "write"
-                thread = thread + "<a id='thread_z_"+str(threadid)+"' class='thread edited "+name+" "+section+"'>"+edit[1]+"</a>"
+                thread = thread + "<a id='thread_yyy_"+str(threadid)+"' class='thread edited "+name+" "+section+"'>"+edit[1]+"</a>"
     ii = ii + 1
         
 #    output.write(container+thread+"""</p></div>""")
